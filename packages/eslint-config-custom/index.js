3846333528a658559b80d5ec82c1115ad8c81219
kon72 <kinsei0916@gmail.com>
2023-06-20 03:40:31 +0900

    Add more ESLint rules

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,10 +1,31 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['next/core-web-vitals', 'turbo', 'prettier'],
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'turbo',
+    'prettier',
+  ],
   plugins: ['prettier'],
   rules: {
-    '@next/next/no-html-link-for-pages': 'off',
+    // Borrowed from gts
     'prettier/prettier': 'error',
+    'block-scoped-var': 'error',
+    eqeqeq: 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'eol-last': 'error',
+    'prefer-arrow-callback': 'error',
+    'no-trailing-spaces': 'error',
+    quotes: ['warn', 'single', {avoidEscape: true}],
+    'no-restricted-properties': [
+      'error',
+      {object: 'describe', property: 'only'},
+      {object: 'it', property: 'only'},
+    ],
+    // Custom rules
+    '@next/next/no-html-link-for-pages': 'off',
   },
   parserOptions: {
     babelOptions: {
