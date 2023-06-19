0ccb92adaf6aefd38d2c5e4a6cd32955c6124c0a
kon72 <kinsei0916@gmail.com>
2023-06-19 19:04:47 +0900

    Add format rules

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,14 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ['next', 'turbo', 'prettier'],
+  plugins: ['prettier'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
+    '@next/next/no-html-link-for-pages': 'off',
+    'prettier/prettier': 'error',
   },
   parserOptions: {
     babelOptions: {
-      presets: [require.resolve("next/babel")],
+      presets: [require.resolve('next/babel')],
     },
   },
 };
