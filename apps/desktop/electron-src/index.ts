487218e486e8743797f888722f2bc95d61fdea53
kon72 <kinsei0916@gmail.com>
2023-06-24 01:10:27 +0900

    Rename standalone to desktop

--- apps/desktop/electron-src/index.ts
+++ apps/desktop/electron-src/index.ts
@@ -9,7 +9,7 @@ import prepareNext from 'electron-next';
 
 // Prepare the renderer once the app is ready
 app.on('ready', async () => {
-  await prepareNext('./apps/standalone/renderer');
+  await prepareNext('./apps/desktop/renderer');
 
   const mainWindow = new BrowserWindow({
     width: 800,
